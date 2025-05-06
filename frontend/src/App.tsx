629e6d7710376442096ba28af3fdb281f3934f63
Radwan Mansur <raydiaz1899@gmail.com>
2025-05-06 14:34:03 +0300

    browser router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
 
-
+import { Routes, Route } from "react-router-dom"
+import { NavBar } from "./components/NavBar"
+import { Layout } from "./components/Layout"
+import { About } from "./components/About"
+import { FormPost } from "./components/FormPost"
 function App() {
 
   return (
-    <>
+    <section className="main-container">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/about" element={<About />} />
+          <Route path="/form" element={<FormPost />} />
 
-    </>
+        </Route>
+
+
+      </Routes>
+    </section>
   )
 }
 
---
+++ frontend/src/components/FormPost.tsx
@@ -0,0 +1,5 @@
+
+
+export function FormPost() {
+    return <div>FormPost</div>;
+}
